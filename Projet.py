9b9f74cc767c0288f1ca129f8c273aaaa0781056
Chaddaï Fouché <chaddai.fouche@gmail.com>
2017-04-17 15:07:04 +0200

    A long time ago, in a classroom far far away...

---
+++ Projet.py
@@ -0,0 +1,27 @@
+﻿# Créé par martelet, le 13/03/2017 en Python 3.2
+import random as rand
+
+Players = []
+Roles = [1,58,69,84]
+
+
+def add_player (playername):
+    p = input("Entrez votre pseudo")
+    Players.append[p]
+
+
+def start_game ():
+    r = random.choice(Roles)
+    Roles.pop(r)
+for i in Roles:
+    start_game()
+    print(i)
+
+def vote( playerName, playerName ):
+    ...
+
+def turn ():
+    ...
+
+def answer( request ):
+    ...
---
+++ Test.py
@@ -0,0 +1,9 @@
+﻿# Créé par martelet, le 13/03/2017 en Python 3.2
+import random
+Roles = [1,58,69,84]
+def start_game ():
+    r = random.choice(Roles)
+    Roles.pop(r)
+for i in Roles:
+    start_game()
+    print(i)
